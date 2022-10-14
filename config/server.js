1b5f0eb3b0063123828de5779292b4179faf9f57
Korpan-Artem <arttodoc@gmail.com>
2022-10-14 11:58:16 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'abe2acd98ec6c1111f1412350690fbb2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ef9d33c-5d23-49a2-afb1-0bc8b3378b56'
+};
